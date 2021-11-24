aeccf09061302d1df853a0cd9a5020f98ded40db
Eduardo <graziolieduardo@gmail.com>
2021-11-24 16:38:17 +0100

    Creazione Auth e prime impostazioni rotte

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('guest.home');
+
+Auth::routes();
+
+// Gestione rotte admin 
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')
+    ->group(function() {
+        Route::get('/', 'HomeController@index')->name('index');
+     });
+
