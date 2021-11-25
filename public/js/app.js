0b92d5cc078c9fb8bcba2c2077277876e181909f
AlbertoZanola <albertozanola1996@gmail.com>
2021-11-25 12:33:07 +0100

    destory e conferma js

--- public/js/app.js
+++ public/js/app.js
@@ -37270,6 +37270,10 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+function confirmDelete() {
+  var resp = confirm('Vuoi cancellare ?');
+}
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -37335,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Eduardo\Documents\BoolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Eduardo\Documents\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\alber\OneDrive\Desktop\Boolean\PHP\BoolBnB\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\alber\OneDrive\Desktop\Boolean\PHP\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
