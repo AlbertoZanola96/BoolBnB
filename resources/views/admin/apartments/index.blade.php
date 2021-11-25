0d86bc5c3e2f899e4e2faeced31ae457e46237a5
Eduardo <graziolieduardo@gmail.com>
2021-11-25 13:58:11 +0100

    Aggiunto admin apartments index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,5 +1,52 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
-    <h1>sono l'index della sezione admin</h1>
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    @if (session('modified'))
+        <div class="alert alert-succes">
+            {{ session('modified') }}
+        </div>
+    @endif
+    @if (session('deleted'))
+        <div class="alert alert-danger">
+            {{ session('deleted') }}
+        </div>
+    @endif
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($apartments as $apartment)
+                <tr>
+                    <th scope="row">{{ $apartment->id }}</th>
+                    <td>{{ $apartment->name }}</td>
+                    <td>{{ $apartment->address }}</td>
+                   
+                    <td>
+                        <a href="">
+                            <button class="btn btn-primary">Details</button>
+                        </a>
+                        <a href="">
+                            <button class="btn btn-warning">Modify</button>
+                        </a>
+                        <form action="" class="deleteForm" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
