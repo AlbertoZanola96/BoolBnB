5f223aea96a5c843d49d9b96fcca8fafcc0ab5d7
ManuelInfante <manuel.i3.infante@gmail.com>
2021-11-29 23:13:35 +0100

    fix merge

--- public/js/app.js
+++ public/js/app.js
@@ -37347,8 +37347,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Mariano\Desktop\BooleanClasse#39\progetto_finale\BoolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Mariano\Desktop\BooleanClasse#39\progetto_finale\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/manuelinfante/Desktop/Boolean/ProgettoFinale/BoolBnB/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/manuelinfante/Desktop/Boolean/ProgettoFinale/BoolBnB/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
