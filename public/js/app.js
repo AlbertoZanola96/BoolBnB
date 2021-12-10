5a4949ba5577f1610b67aa178bb8fa230c4afb5c
Andrea <andreagambinomarketing@gmail.com>
2021-12-10 11:16:51 +0100

    spostamento dashboard

--- public/js/app.js
+++ public/js/app.js
@@ -37355,7 +37355,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-
 __webpack_require__(/*! /Users/andreagambino/Desktop/Boolean 39/BoolBnB/resources/js/app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! /Users/andreagambino/Desktop/Boolean 39/BoolBnB/resources/sass/app.scss */"./resources/sass/app.scss");
 
--- public/js/front.js
+++ public/js/front.js
@@ -22418,7 +22418,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/manuelinfante/Desktop/Boolean/progetto finale/BoolBnB/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/andreagambino/Desktop/Boolean 39/BoolBnB/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
