9379221bf20217f24a86c78064572454e3ccaf51
Eduardo <graziolieduardo@gmail.com>
2021-12-10 17:06:56 +0100

    fix img edit

--- public/js/app.js
+++ public/js/app.js
@@ -37370,6 +37370,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 __webpack_require__(/*! C:\Users\Eduardo\Documents\BoolBnB\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\Eduardo\Documents\BoolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
 
+
 /***/ })
 
 /******/ });
